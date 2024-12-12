15bd0cea7b4563c59746d34bd9e574818725e9c2
avangardewashere <avelpanaligan@Gmail.com>
2024-12-12 16:25:29 +0800

    add/route-post-handler

---
+++ app/api/cat/route.ts
@@ -0,0 +1,31 @@
+import { ICatData } from "@/components/Forms/addCatForm";
+import connectMongoDb from "@/libs/mongoDb";
+import Cat from "@/models/catSchema";
+import { NextResponse } from "next/server";
+export async function POST(req: Request) {
+  try {
+    const data:ICatData = await req.json();
+    const { catName, breed, color } = data;
+
+    await connectMongoDb();
+
+    await Cat.create({ catName, breed, color });
+
+    return NextResponse.json(
+      {
+        message: "A new cat information has been added.",
+        data,
+      },
+      { status: 201 }
+    );
+  } catch (e: any) {
+    console.log("Error: ", e);
+    return NextResponse.json(
+      {
+        message: "Error in creating a new cat information",
+        error: e.message,
+      },
+      { status: 500 }
+    );
+  }
+}
